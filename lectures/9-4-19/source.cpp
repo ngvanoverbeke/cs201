d11a75a8acbcc6f33a96419d0ce22f3e2ec0eb80
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-04 10:16:32 -0800

    changed source from get testing file to working lab file; will square entered number +1

--- lectures/9-4-19/source.cpp
+++ lectures/9-4-19/source.cpp
@@ -1,4 +1,19 @@
 // Nathan VanOverbeke
 // cs201
 // 9/4/19
-// this is a test document for testing git
+// this source is for following along with the lecture
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+int s;
+
+int main() {
+    cout << "Please type an integer" << endl;
+    cin >> n;
+        n++;
+        s = (n*n);
+    cout << s << endl;
+ }
