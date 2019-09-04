104479bd27d91c466e2661285ea9d3b691ecbbd2
ngvanoverbeke <natevo00@gmail.com>
2019-09-04 15:59:54 -0800

    Fixed a typo

--- HW0/Main Program/Source.cpp
+++ HW0/Main Program/Source.cpp
@@ -1,7 +1,7 @@
 // Nathan VanOverbeke
 // cs201
 // 9/3/2019
-// This is the main program of HW0; it simply plrints the given text 5 times
+// This is the main program of HW0; it simply prints the given text 5 times
 
 #include <iostream>
 
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
     // I used a simple while loop instead of copy/pasting "Hello world" like I did in the original version. It is much easier to change the number of times or what is printed while using this design
-    
+
     int n = 0;
 
     while (n <= 4)
