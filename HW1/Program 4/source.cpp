b7bdce4779794835985dbddb75eeb66a87aa79b6
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-06 10:16:21 -0800

    Working on making it repeatable. currently not functioning correctly but still works

--- HW1/Program 4/source.cpp
+++ HW1/Program 4/source.cpp
@@ -3,16 +3,27 @@
 // 9/15/19
 // This program will intake miles from a user and output kilometers
 
-#include <iostream>;
+#include <iostream>
 
 using namespace std;
 
-int m;
-int k;
+double m;
+double k;
+int r;
 int main() {
+    start:
     cout << "Please enter the number of miles you would like to convert" << endl;
     cin>> m;
     k = m * 1.609344;
+    cout << k << endl;
+    cout << "would you like to convert another? Enter 1 for yes or 2 for no." << endl;
+    cin>> r;
+    if (r = 1){
+        goto start;
+    }
 
+    else {
+        exit(EXIT_SUCCESS);
+    }
 
 }
