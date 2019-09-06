d00adc993b3e2f0bc4cb25059f72a0ae9c5d6b70
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-06 09:32:51 -0800

    created program 4, created a main and added some of the needed algorithim

---
+++ HW1/Program 4/source.cpp
@@ -0,0 +1,18 @@
+// Nathan VanOverbeke
+// cs201
+// 9/15/19
+// This program will intake miles from a user and output kilometers
+
+#include <iostream>;
+
+using namespace std;
+
+int m;
+int k;
+int main() {
+    cout << "Please enter the number of miles you would like to convert" << endl;
+    cin>> m;
+    k = m * 1.609344;
+
+
+}
