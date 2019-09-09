cca0a680773a1b63daa66a0a9d1a9b6a0b856676
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-09 09:44:09 -0800

    added comments throughout

--- labs/Lab 3/source 2/source2.cpp
+++ labs/Lab 3/source 2/source2.cpp
@@ -7,18 +7,22 @@
 
 using namespace std;
 
+// These initialized values will be used in the while loops to print the box
 int w = 1;
 int h = 1;
 
 int main()
 {
+    // This will ensure that only 10 lines will be written
     while (h <= 10)
     {
+        // This will ensure there are 60 "@" symbols per line
         while (w <= 60)
         {
             cout << "@";
             w++;
         }
+        // Ends previous line, resets width to 1, and adds 1 to the height, as the previous line has been finished
         cout << endl;
         w = 1;
         h++;
