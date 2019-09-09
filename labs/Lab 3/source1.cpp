075247e4be2ac1c991b5027dfa1755fc97e8cbef
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-09 09:23:32 -0800

    created source and created working main function

---
+++ labs/Lab 3/source1.cpp
@@ -0,0 +1,20 @@
+// Nathan VanOverbeke
+// cs201
+// 9/9/19
+// Print 1-20 by using a while loop
+
+#include <iostream>
+
+using namespace std;
+
+int n = 1;
+
+int main() {
+    while (n <= 20)
+    {
+        cout << n << endl;
+        n++;
+    }
+    return 0;
+    
+}
