6a9493adfb724e2bc27834f8817af9eb8c46e8ea
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-11 10:10:27 -0800

    added comments

--- labs/Lab 4/Aditional/source.cpp
+++ labs/Lab 4/Aditional/source.cpp
@@ -9,27 +9,31 @@ using namespace std;
 
 int main() {
     
+    // ensures we go to 100 and then stop
     for (int n = 1; n <= 100; n++)
     {
-        {    
+        {   
+            // checks if divisible by 3
             if (n % 3 == 0)
             {
+                //checks if also divisible by 5
                 if (n % 5 == 0)
                 {
-                cout << "fizz buzz" << endl;
+                    cout << "fizz buzz" << endl;
                 }
 
                 else{
-                cout << "fizz" << endl;
+                    cout << "fizz" << endl;
                 }
             }
             
+            // checks if divisible by 5
             if (n % 5 == 0)
             {
                 cout << "buzz" << endl;
             }
             
-        
+            // prints the number if it's found to not be divisible by 3 or 5
             else
             {
                 cout << n << endl;
--- labs/Lab 4/Requirment 1/source.cpp
+++ labs/Lab 4/Requirment 1/source.cpp
@@ -11,14 +11,17 @@ int n = 1;
 
 int main() {
     
+    // ensures we count to 100 and then stop
     while (n <= 100)
     {    
+        // checks if divisble by 3
         if (n % 3 == 0)
         {
             cout << "fizz" << endl;
             n++;
         }
-    
+
+        // prints the number if it is not divisible by 3
         else
         {
             cout << n << endl;
