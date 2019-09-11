2fbdd00d2fa8a17727f9945a7205e3b8b27e106d
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-11 09:35:51 -0800

    Created the program and main function

---
+++ HW1/Main Program/source.cpp
@@ -0,0 +1,33 @@
+// Nathan VanOverbeke
+// cs201
+// 9/15/19
+// This program will print a diamond given the size by a user
+
+#include <iostream>
+
+using namespace std;
+
+int d = 0;
+int p = 0;
+
+int main() {
+    cin >> d;
+
+    if (d < 1)
+    {
+        cout << "Sorry, please try a positive number" << endl;
+    }
+
+    else
+    {
+        while (p < d, p++)
+        {
+            
+        }
+        
+    }
+    
+
+
+    
+}
