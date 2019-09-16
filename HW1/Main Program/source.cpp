c9dc28a8326c0c256956bd24ed8b8bf1808835fd
ngvanoverbeke <natevo00@gmail.com>
2019-09-15 23:57:03 -0800

    finished up main function added comments

--- HW1/Main Program/source.cpp
+++ HW1/Main Program/source.cpp
@@ -8,13 +8,17 @@
 using namespace std;
 
 int i = 1; // input
-int r = 1;
+
+int x;
 
 int main() {
-    cout << "Please enter the size of the diamond you would like to create" << endl;
-    cin >> i;
 
-    if (i < 1)
+    // asks the the user for a value but also tells the user what the value will do
+    cout << "Please enter the size of the diamond you would like to create" << endl;
+    cin >> x;
+
+    // checks if the given input is useable and exits if its not
+    if (x < 1)
     {
         cout << "Sorry, please try a positive number" << endl;
         return 0;
@@ -22,5 +26,45 @@ int main() {
 
     else
     {
-        
+        // init value l so that it can be used in the for loop after this one
+        int l;
+
+        // this for loop handles the top half of the diamond
+        for (int i = 0; i <= x; i++)
+        {
+
+            // this for loop handles the spaces on each line that are put in before the hashes start
+            for (int k = 0; k < x - i; k++)
+            {
+                cout << " ";
+            }
+
+            // this for loop outputs the correct number of hashes
+            for (int p = 0; p < 2 * i - 1; p++)
+            {
+                cout << "#";
+            }
+
+            // after the line is finished this ends the line and resets the value of r to the input
+            cout << endl;
+            l = i;
+        }
+
+        // this is the 2nd loop that will handle the bottom half of the diamond by subtracting each time it loops
+        for (int i = l-=1; i > 0; i--)
+        {
+            // this for loop is the same as the one above, it handles the spaces before the hashes
+            for (int k = 0; k < x - i; k++)
+            {
+                cout << " ";
+            }
+
+            // this 
+            for (int p = 0; p < 2 * i - 1; p++)
+            {
+                cout << "#";
+            }
+            cout << endl;
+        }
     }
+}
