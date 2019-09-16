418ca3be7601a1afdab891ac3b91c6093bba0e88
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-16 10:11:08 -0800

    using getline and size; created the program

--- labs/Lab 06/main.cpp
+++ labs/Lab 06/main.cpp
@@ -4,4 +4,12 @@
 // This program will do a number of different tasks as defined for lab 06
 
 #include <iostream>
+#include <string>
 
+int main () {
+    std::string i;
+    std::cout << "please type a string:  ";
+    std::getline(std::cin, i);
+    std::cout << "your string was: " << i << " and it was " << i.size() << " characters long.";
+    return 0;
+}
