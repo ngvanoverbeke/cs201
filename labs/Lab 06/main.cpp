4992b961b5fed90e50499078d911a416c1ae6131
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-16 10:12:43 -0800

    improved the description comment

--- labs/Lab 06/main.cpp
+++ labs/Lab 06/main.cpp
@@ -1,7 +1,7 @@
 // Nathan VanOverbeke
 // cs201
 // 9/16/19
-// This program will do a number of different tasks as defined for lab 06
+// This program will do a number of different tasks as defined for lab 06, including taking a string from the user and outputting it with the number of characters they used
 
 #include <iostream>
 #include <string>
