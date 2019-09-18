8849703cb1cf3065a86d3b6a1f781e91ecc2f1aa
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-18 09:52:00 -0800

    introduced intio files and created main.cpp

---
+++ labs/Lab 07/Main/main.cpp
@@ -0,0 +1,14 @@
+// Nathan VanOverbeke
+// cs201
+// 9/18/19
+// This program will interface with intio.cpp and intion.hpp to make them do what we want
+
+#include <iostream>
+#include "intio.hpp"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    
+}
