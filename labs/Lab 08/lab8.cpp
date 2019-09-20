d5030bc4eab9e8c5a3bfce8e9d341ec87a9a517a
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-20 10:10:30 -0800

    added functions to lab8; updated tasks.json

---
+++ labs/Lab 08/lab8.cpp
@@ -0,0 +1,20 @@
+// Nathan VanOverbeke
+// CS 201
+// 9/20/2019
+// Lab 8
+
+// #include "std_lib_facilities.hpp"
+#include "lab8.hpp"
+#include "intio.hpp"
+
+int doinput(){
+    int x = getInt();
+
+    return (x);
+
+}
+    
+int compute(int anything){
+    anything = anything * anything;
+    return (anything);
+}
