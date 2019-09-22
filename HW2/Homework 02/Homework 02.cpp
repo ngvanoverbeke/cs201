69093711c3d86ca41a290c20325b75222015c330
ngvanoverbeke <natevo00@gmail.com>
2019-09-22 15:45:42 -0800

    Moved the input functionality to its own function

--- HW2/Homework 02/Homework 02.cpp
+++ HW2/Homework 02/Homework 02.cpp
@@ -9,24 +9,25 @@
 #include <string>
 #include <algorithm>
 
-
-int main(int argc, char **argv)
-{
-	
-	void InputNames(std::vector<std::string> names);
-	return 0;
-
-}
-
-void InputNames(std::vector<std::string>& names)
+void InputNames()
 {
 
+	std::vector<std::string> names;
 	for (int i = 0; i < 10; i++)
 	{
 		std::string name;
 		std::cout << "Please type a name:	";
 		std::getline(std::cin, name);
 		names.push_back(name);
+
 	}
 }
 
+
+int main(int argc, char **argv)
+{
+	InputNames();
+
+	return 0;
+
+}
