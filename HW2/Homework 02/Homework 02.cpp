883ff28d1188794b38e526c589a7b1b82cf11d10
ngvanoverbeke <natevo00@gmail.com>
2019-09-22 19:10:48 -0800

    split create and search into 2 functions created working printNames function

--- HW2/Homework 02/Homework 02.cpp
+++ HW2/Homework 02/Homework 02.cpp
@@ -9,24 +9,65 @@
 #include <string>
 #include <algorithm>
 
-void InputNames()
-{
 
+std::vector<std::string> inputNames()
+{
 	std::vector<std::string> names;
+	std::string pulled;
+
 	for (int i = 0; i < 10; i++)
 	{
-		std::string name;
-		std::cout << "Please type a name:	";
-		std::getline(std::cin, name);
-		names.push_back(name);
+		std::cout << "Enter a name: ";
+		getline(std::cin, pulled);
+		names.push_back(pulled);
+	}
 
+	return names;
+}
+
+void nameSearch(std::vector<std::string> x) {
+	std::vector<std::string> namelist = x;
+	std::string localName;
+	std::cout << "Name Search:	";
+	getline(std::cin, localName);
+
+	int appears = 0;
+
+	for (int i = 0; i < 10; i++) {
+		if (localName == namelist.at(i)) {
+			appears++;
+		}
+	}
+
+	if (appears > 1) {
+		std::cout << "That name apears " << appears << " times" << std::endl;
+	}
+	else if (appears == 1) {
+		std::cout << "That name appears once" << std::endl;
+	}
+	else {
+		std::cout << "Sorry. That name did not appear" << std::endl;
+	}
+}
+
+void printNames(std::vector<std::string> x) {
+	std::vector<std::string> namelist = x;
+	for (int i = 0; i < 10; i++) {
+		std::cout << namelist.at(i);
 	}
 }
 
 
+
 int main(int argc, char **argv)
 {
-	InputNames();
+	std::vector<std::string> namelist = inputNames();
+	std::string name;	
+
+	nameSearch(namelist);
+
+	printNames(namelist);
+
 
 	return 0;
 
