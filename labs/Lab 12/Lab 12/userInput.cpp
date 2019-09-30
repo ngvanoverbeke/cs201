ff9b060aa290d517cd77202c1d6759d0188d7992
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-09-30 10:16:18 -0800

    added/created files as well as beginnings of the getUserStrings function

---
+++ labs/Lab 12/Lab 12/userInput.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+ void getUserStrings(std::vector<std::string>& words) {
+	 std::string inputWord;
+	 while (inputWord != "end")
+	 {
+		 std::cin >> inputWord;
+	 }
+	 
+}
