dd2093713690721cabede140ee0c614c74303773
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-10-14 11:16:39 -0800

    created 3 main files and a main function

---
+++ HW4/HW4/tokenizer.cpp
@@ -0,0 +1,18 @@
+// Nathan VanOverbeke
+// CS 201
+// HW 4
+// 10/13/2019
+// This program will take input from the user; a bunch of text that ends with "end"
+// and it will, if possible, tokenize the text into seperate strings/identifiers
+
+#include "tokenizer.hpp";
+#include <iostream>;
+#include <string>;
+
+
+int main() {
+	std::string userISring;
+	std::cout << "Please type some text. When done type end/End/END" << std::endl;
+	//std::getline (std::cin, userIString);
+	//isToken();
+}
