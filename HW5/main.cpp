701841c8c538c3163cfa4eb31fe79ec9babb1ef9
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-11-13 01:26:47 -0900

    made input boxes automatic (req'd pressing enter before)
    cleaned up naming

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -7,7 +7,6 @@
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Output.H>
-#include <iostream>
 #pragma comment(lib, "fltk.lib")
 #pragma comment(lib, "comctl32.lib")
 
@@ -20,23 +19,22 @@ Fl_Input* inStr = new Fl_Input(120, 40, 240, ControlHeight, "Type a string");
 Fl_Input* inInt = new Fl_Input(120, 80, 240, ControlHeight, "how much?\n (int)");
 Fl_Output* truncResult = new Fl_Output(120, 120, 240, ControlHeight, "trunc result");
 
-void OnCloseCallback(Fl_Widget* w, void* data) {
+void onCloseCallback(Fl_Widget* w, void* data) {
 	Fl_Window* window = (Fl_Window*)data;
 	if (window) window->hide();
 }
 
-void TruncButtonCallback(Fl_Widget* w, void* data) {
-	auto MyTempStruct = trunc(_string);
-	truncResult->value (MyTempStruct.str.c_str());
+void truncButtonCallback(Fl_Widget* w, void* data) {
+	auto StructResult = trunc(_string);
+	truncResult->value (StructResult.str.c_str());
 }
 
-void DeyBTyping(Fl_Widget* w) {
+void inStringCallback(Fl_Widget* w) {
 	_string.str = inStr->value();
 }
 
-void giveMeNumbrPls(Fl_Widget* w) {
+void inIntCallback(Fl_Widget* w) {
 	_string.len = std::atoi(inInt->value());
-	std::cout << _string.len << std::endl;
 }
 
 
@@ -44,14 +42,14 @@ int main(int argc, char** argv) {
 	_string.len = 0;
 	_string.str = "";
 	// *******Callbacks*******
-	inStr->when(FL_WHEN_ENTER_KEY);
-	inStr->callback(DeyBTyping);
+	inStr->when(FL_WHEN_CHANGED);
+	inStr->callback(inStringCallback);
 	
-	inInt->when(FL_WHEN_ENTER_KEY);
-	inInt->callback(giveMeNumbrPls);
+	inInt->when(FL_WHEN_CHANGED);
+	inInt->callback(inIntCallback);
 
-	truncBtn->callback(TruncButtonCallback, (void*)window);
-	closeBtn->callback(OnCloseCallback, (void*)window);
+	truncBtn->callback(truncButtonCallback, (void*)window);
+	closeBtn->callback(onCloseCallback, (void*)window);
 
 // *******How It's Made*******
 	inStr->labelfont(FL_BOLD);
