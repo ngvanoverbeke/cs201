a188c03c498b041bfb367e41591a819b97b16a58
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-11-15 18:55:43 +0000

    addes give pointers.cpp file to new Lab30 folder

---
+++ labs/Lab30/pointers.cpp
@@ -0,0 +1,59 @@
+// pointers.cpp
+// Glenn G. Chappell
+// 9 Apr 2018
+//
+// For CS 201 Spring 2018
+// Programming with Pointers
+//
+// Modified 11/14/18
+// Chris Hartman
+// For CS 201 Fall 2018
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+// changeInt
+// Given a pointer to int, change the value pointed to.
+void changeInt(int * p)  // p points to value to change
+{
+    // WRITE THIS!!!
+}
+
+
+int main()
+{
+    int ss = 3;
+    int tt = 4;
+
+    cout << "START" << endl;
+    cout << "Value of ss: " << ss << endl;
+    cout << "Value of tt: " << tt << endl;
+    cout << endl;
+
+    int * ip;
+
+    // *** HERE, ADD CODE THAT USES ip to modify variables ss and tt
+
+    cout << "AFTER USING ip" << endl;
+    cout << "Value of ss: " << ss << endl;
+    cout << "Value of tt: " << tt << endl;
+    cout << endl;
+
+    // *** UNCOMMENT AND COMPLETE THE FOLLOWING LINE
+
+    //changeInt( ??? );  // Pass a pointer to ss to function changeInt
+
+    cout << "AFTER CALLING changeInt" << endl;
+    cout << "Value of ss: " << ss << endl;
+    cout << endl;
+
+    // Wait for user
+    cout << "PRESS ENTER ";
+    while (cin.get() != '\n') ;
+
+    return 0;
+}
+
