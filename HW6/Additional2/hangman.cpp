f4295da4aff911c8d4c718ed038560bfa55b5ccf
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-11-18 11:47:35 +0000

    mostly completed userInput function -created vector of chars for the game's words

--- HW6/Additional2/hangman.cpp
+++ HW6/Additional2/hangman.cpp
@@ -9,8 +9,40 @@
 #include <algorithm>
 #include <map>
 #include <random>
+
+using std::begin;
+using std::end;
+using std::find;
+
 std::pair<char, bool> charPair;
-std::map<char, bool> charInventory;
+std::map<char, bool> charInventory; // DONT TOUCH THIS!!
+
+std::vector<std::vector<char>> victor{
+    {'f','o','u','r'},
+    {'f','i','v','e'},
+    {'f','a','i','l'},
+    {'f','o','r','d'},
+    {'f','o','a','l'},
+    {'f','o','u','l'}};
+
+std::vector<char> bird;
+
+// takes input of a letter from the user and checks if they have tried it yet
+void userInput(){
+    char inputChar;
+    std::cin >> inputChar;
+    auto temp = find(begin(charInventory), end(charInventory), inputChar);
+    if (temp == end(charInventory)){
+        std::cout << std::endl << "Hey dummy, you already tried that! Try something different." << std::endl;
+        userInput();
+    }
+
+    charPair.first = inputChar;
+    charPair.second = true;
+
+    charInventory.insert(charPair);
+    
+}
 
 void failedAttempt(){
     // this function will print the current hangman graphic based on number of past 
