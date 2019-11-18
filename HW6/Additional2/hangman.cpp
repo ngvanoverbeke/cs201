937d6c80d15b343b38034b225d0ac70ca78ee418
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-11-18 11:09:44 +0000

    initial commit

---
+++ HW6/Additional2/hangman.cpp
@@ -0,0 +1,21 @@
+// Nathan VanOverbeke
+// CS201 HW6: additiona; program 2 (hangman game)
+// 11/17/19
+// This will be a basic hangman game using a map to keep track of words used and no for/while loops
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <random>
+std::pair<char, bool> charPair;
+std::map<char, bool> charInventory;
+
+void failedAttempt(){
+    // this function will print the current hangman graphic based on number of past 
+}
+
+int main(){
+    
+}
