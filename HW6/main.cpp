5f9b508158da1f491c73773f4e80ae535cbae48c
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-11-18 09:38:42 +0000

    completed all but int RandomBetween() -Currently runs slow due to large sample size; may add multithreading

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -4,27 +4,23 @@
 #include <map>
 #include <random>
 #include <cmath>
- 
-int RandomBetweenU(int first, int last){
-    std::random_device r;
 
-    // Choose a random mean between 1 and 6
-    std::default_random_engine e1(r());
+std::random_device r;
+std::default_random_engine e1(r());
+
+int RandomBetweenU(int first, int last){
     std::uniform_int_distribution<int> uniform_dist(first, last);
     int mean = uniform_dist(e1);
     return mean;
 }
 
 int RandomBetweenN(int first, int last){
-    std::random_device r;
-    std::default_random_engine e1(r());
     std::uniform_int_distribution<int> uniform_dist(first, last);
     int mean = uniform_dist(e1);
-    // Choose a random mean between 1 and 6
     std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()}; 
     std::mt19937 e2(seed2);
     std::normal_distribution<> normal_dist(mean, 2);
-    return mean;
+    return std::round(normal_dist(e2));
 }
 
 void PrintDistribution(const std::map <int, int> &hist){
@@ -37,7 +33,7 @@ void PrintDistribution(const std::map <int, int> &hist){
 int main()
 {
     std::map<int, int> histN;
-    for (int n = 0; n < 10000; ++n) {
+    for (int n = 0; n < 100000; ++n) {
         ++histN[RandomBetweenN(1, 6)];
     }
     std::cout << "Normal distribution of random number N" << std::endl;
@@ -45,7 +41,7 @@ int main()
     std::cout << std::endl;
 
     std::map<int, int> histU;
-    for (int n = 0; n < 10000; ++n) {
+    for (int n = 0; n < 100000; ++n) {
         ++histU[RandomBetweenU(1, 6)];
     }
     std::cout << "Uniform distribution of random number U" << std::endl;
