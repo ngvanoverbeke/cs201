754cfb38b27a4023c8663ed79c4de79705496cd3
Nathan VanOverbeke <ngvanoverbeke@alaska.edu>
2019-11-22 09:45:27 +0000

    completed fully, just need comments

--- HW6/Additional2/hangman.cpp
+++ HW6/Additional2/hangman.cpp
@@ -12,12 +12,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+void draw();
+
 using std::begin;
 using std::end;
 using std::find;
 
 std::pair<char, bool> charPair;
 std::map<char, bool> charInventory; // DONT TOUCH THIS!!
+
+int WRONG = 0;
+
 
 std::vector<std::vector<char>> victor{
     {'f','o','u','r'},
@@ -34,13 +39,16 @@ std::vector<char> userGuess{
 
 
 void failedAttempt(){
-    // this function will print the current hangman graphic based on number of past 
+    draw();
+    std::cout << userGuess[0] << userGuess[1] << userGuess[2] << userGuess[3] << std::endl;
+
 }
 
 
 // takes input of a letter from the user and checks if they have tried it yet
 void userInput(){
     std::cout << "Hey you! Yeah you. Guess a letter!" << std::endl;
+
     char inputChar;
     std::cin >> inputChar;
     charPair.first = inputChar;
@@ -57,7 +65,7 @@ void userInput(){
         auto tempW = find(begin(gameWord), end(gameWord), inputChar);
         if (tempW == end(gameWord)){
             std::cout << std::endl << "WRONG!!" << std::endl;
-            failedAttempt();
+            WRONG++;
             return;
         }
         int position = std::distance(gameWord.begin(), tempW);
@@ -67,19 +75,157 @@ void userInput(){
     }
 }
 
-
-
 int main(){
     srand(time(NULL));
-    int gameSeed = std::rand() % 6;
+    int gameSeed = std::rand() % 6; // NOTE: to add more words, simply make sure this number (6 by default) matches the number of words you have!
     gameWord = victor[gameSeed];
     bool playing = (std::equal(gameWord.begin(), gameWord.end(), userGuess.begin()));
+
     while (!playing)
     {
         userInput();
+        //std::cout << userGuess << std::endl;
         playing = (std::equal(gameWord.begin(), gameWord.end(), userGuess.begin()));
+
+        if (WRONG == 10){
+            failedAttempt();
+            std::cout << "You get nothing! You lose, good day sir!" << std::endl;
+            break;
+        }
+    failedAttempt();
+    }
+    if(playing){
+        std::cout << "You won. Now, for something completely different." << std::endl;   
     }
     
-    std::cout << "You won. Now, for something completely different." << std::endl;
     
+}
+
+
+void draw(){
+    switch (WRONG)
+    {
+    case 1:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |      (_)" << std::endl <<
+        " |         " << std::endl <<
+        " |        " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+
+    case 2:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |      (_)" << std::endl <<
+        " |       |  " << std::endl <<
+        " |       | " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 3:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |      (_) " << std::endl <<
+        " |      \\|  " << std::endl <<
+        " |       | " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 4:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |      (_)" << std::endl <<
+        " |      \\|/  " << std::endl <<
+        " |       | " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 5:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |     _(_)" << std::endl <<
+        " |      \\|  " << std::endl <<
+        " |       | " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 6:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |     _(_)_" << std::endl <<
+        " |      \\|/  " << std::endl <<
+        " |       | " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 7:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |     _(_)_" << std::endl <<
+        " |      \\|/  " << std::endl <<
+        " |       | " << std::endl <<
+        " |      /  " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 8:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |     _(_)_" << std::endl <<
+        " |      \\|/  " << std::endl <<
+        " |       | " << std::endl <<
+        " |      / \\ " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 9:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |     _(_)_" << std::endl <<
+        " |      \\|/  " << std::endl <<
+        " |       | " << std::endl <<
+        " |     _/ \\ " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    case 10:
+        std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |     _(_)_" << std::endl <<
+        " |      \\|/  " << std::endl <<
+        " |       | " << std::endl <<
+        " |     _/ \\_ " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    default:
+     std::cout <<
+        "  _______" << std::endl <<
+        " |/      |" << std::endl <<
+        " |         " << std::endl <<
+        " |         " << std::endl <<
+        " |        " << std::endl <<
+        " |         " << std::endl <<
+        " |" << std::endl <<
+        "_|___" << std::endl;
+        break;
+    }
 }
