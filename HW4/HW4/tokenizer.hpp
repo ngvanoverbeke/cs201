884ab4c96c5842f0cd870fe8122dfca11c3b8111
ngvanoverbeke <natevo00@gmail.com>
2019-12-10 17:30:03 -0900

    added comments for clarity

--- HW4/HW4/tokenizer.hpp
+++ HW4/HW4/tokenizer.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 
+//see tokenizer.cpp for description of these functions
 bool readLine(std::string & str);
 unsigned int stringToTokenWS(const std::string& input, std::vector<std::string>& tokens);
 void AnalyzeTokens(const std::vector<std::string>& tokens);
--- HW4/HW4/tokenizertest.cpp
+++ HW4/HW4/tokenizertest.cpp
@@ -1,5 +1,6 @@
 #include "tokenizer.hpp"
 
+// uses getline to return a bool and makes string useable by other functions
 bool readLine(std::string& str) {
 	
 	std::getline(std::cin, str);
@@ -11,6 +12,7 @@ bool readLine(std::string& str) {
 	}
 }
 
+// seperates a string into a given vector of strings, again, setting up for future functions
 unsigned int stringToTokenWS(const std::string &input, std::vector<std::string>& tokens) {
 	unsigned int total = tokens.size();
 	std::istringstream tempStream(input);
@@ -22,6 +24,8 @@ unsigned int stringToTokenWS(const std::string &input, std::vector<std::string>&
 	return total;
 }
 
+// this does all the fancy work of figuring out what each string within the vector is;
+// is will tell you if it is whitespace, an identifier, an integer, a string, or other
 void AnalyzeTokens(const std::vector<std::string>& tokens) {
 	int loop = tokens.size();
 	bool num = true;
